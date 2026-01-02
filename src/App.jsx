1fddace52124387a8437f259bbbb6344a6e59fd3
tusharjain2001 <tusharjainqwerty@gmail.com>
2026-01-02 22:51:21 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='text-red-500 font-family-instrument '>
-      tushar
-    </div>
-  )
+    <>
+      <div className="text-red-500 font-family-instrument ">tushar</div>
+      <h1 className="text-4xl font-bold font-helvetica">
+        Helvetica Now Display (Tailwind v4)
+      </h1>
+
+      <p className="font-medium">
+        Companies spend over $100K every year on one senior{" "}
+      </p>
+    </>
+  );
 }
 
-export default App
+export default App;
