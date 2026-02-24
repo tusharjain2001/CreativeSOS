c5520a9682966e83cf7e1b4e171e56754539c598
Tushar Jain <tushar.j@rrhaitech.com>
2026-02-24 19:52:56 +0530

    removed broder from fusion

--- src/Components/SectionRenderers/SectionRendererFusion.jsx
+++ src/Components/SectionRenderers/SectionRendererFusion.jsx
@@ -173,7 +173,7 @@ export default function SectionRendererFusion({ project, sectionName }) {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="bg-white border border-gray-200 rounded-xl"
+        className="bg-white rounded-xl"
       >
         <img
           src={data?.image}
